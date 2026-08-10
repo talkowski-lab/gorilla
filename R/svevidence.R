d02fab64c06bd53f479260e7fcb0f09e64d0109c
Justin Lim <jlim21@mgh.harvard.edu>
2026-08-10 17:11:19 -0400

    svevidence: fix breakpoint SR comparison
    The check for SR support in DELs and DUPs simply took the appropriately
    clipped reads and compared the positions to check that order of the
    clipping positions was also appropriate. However, checking for any SR
    support requires checking all combinations of clipping positions.

--- R/svevidence.R
+++ R/svevidence.R
@@ -345,7 +345,8 @@ check_dup <- function(x, sample_id) {
     sample_sr <- x$sr$mat[sample_id == target & contig == x$region$contig & count >= MIN_SR_SUPPORT, ]
     left_sr <- sample_sr[abs(x$region$start - pos) <= MAX_SR_BREAKPOINT_DIST & side == "left", ]
     right_sr <- sample_sr[abs(x$region$end - pos) <= MAX_SR_BREAKPOINT_DIST & side == "right", ]
-    has_sr_support <- nrow(left_sr) > 0 && nrow(right_sr) > 0 && any(left_sr$pos <= right_sr$pos)
+    sr_combo <- expand.grid(left_sr$pos, right_sr$pos)
+    has_sr_support <- nrow(left_sr) > 0 && nrow(right_sr) > 0 && any(sr_combo$Var1 <= sr_combo$Var2)
 
     svlen <- region$end - region$start + 1
     if (svlen <= SMALL_CNV_MAX) {
@@ -399,7 +400,8 @@ check_del <- function(x, sample_id) {
     sample_sr <- x$sr$mat[sample_id == target & contig == x$region$contig & count >= MIN_SR_SUPPORT, ]
     left_sr <- sample_sr[abs(x$region$start - pos) <= MAX_SR_BREAKPOINT_DIST & side == "right", ]
     right_sr <- sample_sr[abs(x$region$end - pos) <= MAX_SR_BREAKPOINT_DIST & side == "left", ]
-    has_sr_support <- nrow(left_sr) > 0 && nrow(right_sr) > 0 && any(left_sr$pos <= right_sr$pos)
+    sr_combo <- expand.grid(left_sr$pos, right_sr$pos)
+    has_sr_support <- nrow(left_sr) > 0 && nrow(right_sr) > 0 && any(sr_combo$Var1 <= sr_combo$Var2)
 
     svlen <- region$end - region$start + 1
     if (svlen <= SMALL_CNV_MAX) {
